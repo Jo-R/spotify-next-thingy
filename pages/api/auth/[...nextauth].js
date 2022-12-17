452f821319c530141cf4d2ce85c2883b90d4d969
jo <jo.rigg8@gmail.com>
2022-12-17 17:09:18 +0000

    starter code from tutorial

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+// TODO TS!
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private",
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.user = user;
+      session.token = token;
+      return session;
+    },
+  },
+});
