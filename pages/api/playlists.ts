24cef5de12985405980da631e0845dcccf0a1a9f
jo <jo.rigg8@gmail.com>
2022-12-18 07:47:55 +0000

    rename, comment

--- pages/api/playlists.ts
+++ pages/api/playlists.ts
@@ -2,10 +2,12 @@ import { getUsersPlaylists } from "../../lib/spotify";
 import { getSession } from "next-auth/react";
 import { NextApiRequest, NextApiResponse } from "next";
 
+// TODO can type the responses with <> on reponse
+// TODO handle !data case
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = await getSession({ req });
-  if (data) {
-    const response = await getUsersPlaylists(data.token.accessToken);
+  const session = await getSession({ req });
+  if (session) {
+    const response = await getUsersPlaylists(session.token.accessToken);
     const { items } = await response.json();
 
     return res.status(200).json({ items });
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -2,10 +2,12 @@ import { getUserDetails } from "../../lib/spotify";
 import { getSession } from "next-auth/react";
 import { NextApiRequest, NextApiResponse } from "next";
 
+// TODO can type the responses with <> on reponse
+// TODO handle !data case
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = await getSession({ req });
-  if (data) {
-    const response = await getUserDetails(data.token.accessToken);
+  const session = await getSession({ req });
+  if (session) {
+    const response = await getUserDetails(session.token.accessToken);
     const user = await response.json();
 
     return res.status(200).json(user);
